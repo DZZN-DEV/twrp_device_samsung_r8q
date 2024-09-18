48a2201c1d2a3abd41d10cdbec7c0f41c3f03d5e
DZZN-DEV <93328813+DZZN-DEV@users.noreply.github.com>
2024-09-18 03:33:01 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,18 @@
 #
 
 LOCAL_PATH := device/samsung/r8q
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+BOARD_USES_QCOM_FBE_DECRYPTION := true
+BOARD_USES_QCOM_DECRYPTION := true
+
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
